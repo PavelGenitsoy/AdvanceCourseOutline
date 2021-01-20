5530a65ba3e70bec60e0aa539cd70c85bbcb0abf
Pavel <pavlogenizoy@gmail.com>
2021-01-21 01:52:25 +0200

    added info about forward

---
+++ std_forward/Source.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+
+void test1(int& data) {
+	std::cout << __FUNCSIG__ << std::endl;
+}
+
+void test1(int&& data) {
+	std::cout << __FUNCSIG__ << std::endl;
+}
+
+template <typename T>
+void test(T&& data) {
+	std::cout << __FUNCSIG__ << std::endl;
+	test1(std::forward<T>(data));  // forward говорит, что в зависимости какой пришёл Т, я буду вызывать правильную перегрузку метода test1
+}
+
+
+int main(int argc, const char *argv[]) {
+	int a;
+
+	test(10);
+	test(a);
+	test(std::move(a));
+
+	system("pause");
+	return 0;
+}
