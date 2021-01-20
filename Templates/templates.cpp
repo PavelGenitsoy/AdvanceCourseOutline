0f29d8be5fb69a6a003a61a57e276496d041517e
Pavel <pavlogenizoy@gmail.com>
2021-01-20 22:40:18 +0200

    added main info about templates

--- Templates/templates.cpp
+++ Templates/templates.cpp
@@ -1,8 +1,23 @@
 #include <iostream>
+#include <vector>
+
+
+template <typename T>
+class Test {
+public:
+	Test(T t) : data{t} {}
+private:
+	T data;
+};
+
+// deduction guide
+// Test::Test(const char*) -> Test::Test(const std::string&); // работает начиная с С++17 , но почему-то у меня не поддерживается
 
 
 int main(int argc, const char *argv[]) {
-
+	
+	Test t{ "const char" };
+	std::vector v{1,2,3};  // в С++17 можно явно не указывать <int>
 
 	return 0;
 }
