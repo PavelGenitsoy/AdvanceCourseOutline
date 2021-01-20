c453eed15c9b765afdf8636b8590d5a114e9cc2d
Pavel <pavlogenizoy@gmail.com>
2021-01-21 01:51:21 +0200

    added info about universal refference in template class

---
+++ UniversalRef_in_TemplateClass/Source.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+
+template <typename T>   //  T - является частью всего класса
+class Test {
+public:
+	template <typename U>
+	void test(U&& data) {   // шаблонизируем сам метод. U часть только этого метода и эта U&& сущность получается той же универсальной ссылкой
+		std::cout << __FUNCSIG__ << std::endl;
+	}
+};
+
+int main(int argc, const char *argv[]) {
+	Test<int> t;
+	int a;
+
+	t.test(a);
+
+	return 0;
+}
