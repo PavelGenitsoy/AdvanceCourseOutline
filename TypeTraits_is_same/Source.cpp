ea85d99909226ee451e9ad6df94aa2296079d018
Pavel <pavlogenizoy@gmail.com>
2021-01-24 19:32:26 +0200

    added custom type traits is_same

---
+++ TypeTraits_is_same/Source.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <type_traits>
+
+
+template <typename T1, typename T2>
+struct is_same {   // этот use keys работает тогда, когда Т1 и Т2 разные
+	// enum{value = false};  // это старая запись
+	static constexpr bool value{ false };
+};
+
+template <typename T1>
+struct is_same <T1, T1> {   // этот use keys работает тогда, когда Т1 и Т2 одинаковые
+	static constexpr bool value{ true };
+};
+
+int main(int argc, const char *argv[]) {
+
+	std::cout << std::boolalpha;
+
+	std::cout << is_same<int, int>::value << std::endl;
+	std::cout << is_same<int, bool>::value << std::endl;
+
+
+	system("pause");
+	return 0;
+}
