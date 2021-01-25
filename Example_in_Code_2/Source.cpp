901d05231309e9099b5cce4a6c8fe83b20a1fc1f
Pavel <pavlogenizoy@gmail.com>
2021-01-25 18:04:49 +0200

    added last example with templates

---
+++ Example_in_Code_2/Source.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <type_traits>
+
+
+template <typename T>
+class Base {
+public: 
+	void test() {}
+};
+
+/*
+template <typename T>
+class Derived : Base<T> {
+public:				// здесь проблема зависимости контекста.  Есть же компиляция шаблона и потом компиляция при инстанцировании,
+	void calc() {	// тут первого рода ошибка. Компилятор не знает, что такое test(), ибо он не знает, что такое Base<T>, а будет
+		test();		// знать только тогда, когда явно укажем ему этот тип
+	}			  
+};				  
+*/
+
+template <typename T>
+class Derived : Base<T> {
+public:
+	void calc() {
+		this->test();		 //  использование явного this
+		// Base<T>::test();  //  либо так
+	}
+};
+
+int main(int argc, const char *argv[]) {
+
+	Derived<int> d;
+
+
+	system("pause");
+	return 0;
+}
