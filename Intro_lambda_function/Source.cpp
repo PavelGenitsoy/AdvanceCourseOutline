5ecef01e64ed14d622122044af9860b9123c65e9
Pavel <pavlogenizoy@gmail.com>
2021-01-30 03:10:13 +0200

    fix#2

--- Intro_lambda_function/Source.cpp
+++ Intro_lambda_function/Source.cpp
@@ -26,7 +26,7 @@ using Func =  bool(*)(int);  // т.е. Func собой представляет 
 // теперь для шаблонов, тут гараздо приятнее передовать указатели на ф-ции
 template <typename T, typename T1>
 T find_(T begin, T const end, T1 func) {
-	while (begin != end)
+	while (begin != end + 1)
 	{
 		if (func(*begin)) {
 			return begin;
@@ -40,15 +40,16 @@ bool is_even(int value) {  // bool (int)
 	return value % 2 == 0;
 }
 
+
 int main(int argc, const char* argv[]) {
 
 	// std::array<int, 4> arr{1, 2, 3, 4};
-	std::array arr{ 1, 2, 3, 4 };
+	std::array arr{ 1, 1, 3, 4 };
 
 	auto it = find_(&arr[0], &arr[arr.size() - 1], is_even); // а если бы хотели получить число которое делится по модуля 2. то надо передовать в ф-цию предикат
 
 
-	if (it == &arr[arr.size() - 1]) {
+	if (it == &arr[arr.size() - 1] + 1) {
 		std::cout << "Not found\n";
 	}
 	else {
