a21189946083e4eb57fcd3ef7140ccfa3619497a
Pavel <pavlogenizoy@gmail.com>
2021-02-24 21:25:54 +0200

    added info about std::array

---
+++ std_array/Source.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <array>
+
+
+void func(const std::array<int, 10>& arr) {}
+
+
+int main(int argc, const char* argv) {
+
+	// int arr[argc]; // это расширение компилятора, массивы можно делать перемнной длины (сишные массивы)
+	std::array<int, 10> arr{ 1,2,3 };
+
+	// std::array<std::array<int, 10>, 10> arr{1,2};
+
+	std::array<int, 10> arr_{ std::move(arr) }; // copy
+
+
+
+	return 0;
+}
