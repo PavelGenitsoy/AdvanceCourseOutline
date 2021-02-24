82eac0baf538c6aeb73d2d3816e4ebb99b131fed
Pavel <pavlogenizoy@gmail.com>
2021-02-24 20:07:41 +0200

    added info about thread local storage

---
+++ thread_local_storage/Source.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <thread>
+
+
+// thread_local добавляет так называемый storage duration. Есть разные storage duration: avtomatic, global, heap, thread. 
+thread_local int test{}; // делаем переменную, которая будет глобальная между потоками, по сути каждый поток (что будет стучатся к ней) будет иметь свою копию
+
+
+int main(int argc, const char* argv) {
+
+
+
+
+	return 0;
+}
