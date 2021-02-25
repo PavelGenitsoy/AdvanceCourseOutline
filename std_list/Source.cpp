9e65b7c830669ef4b5e55d1b98a396bb9848569a
Pavel <pavlogenizoy@gmail.com>
2021-02-25 17:44:46 +0200

    added some info about std::list

---
+++ std_list/Source.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <list>
+
+
+struct Node { // стандартная реализация двухсвязного списка
+	int data;
+	Node* left;
+	Node* right;
+};
+
+
+
+int main(int argc, const char* argv) {
+
+
+	return 0;
+}
