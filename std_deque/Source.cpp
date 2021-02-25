b91d488e6e4766a0101c2da4a2c6493fc92e4b44
Pavel <pavlogenizoy@gmail.com>
2021-02-25 17:10:48 +0200

    added info about std::deque

---
+++ std_deque/Source.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <deque>
+
+/*
+	API дэки такой же как у вектора за исключением capacity и с дополнением работы в начале.
+*/
+
+int main(int argc, const char* argv) {
+
+	std::deque<int> d{ 1,2,3 };
+
+	for (int i = 0; i < d.size(); ++i)
+	{
+		std::cout << d[i] << " ";
+	}
+
+	system("pause");
+	return 0;
+}
