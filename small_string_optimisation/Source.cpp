ba879561ef5223adaba7d1f6bb47214c13ba41a1
Pavel <pavlogenizoy@gmail.com>
2021-03-03 22:24:37 +0200

    added info about  small string optimisation

---
+++ small_string_optimisation/Source.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <string>
+
+
+void* operator new(size_t count) {
+	std::cout << "new\n";
+	return malloc(count);
+}
+
+
+// void test(const std::string& str) {} // так делали раньше, а после с++17 стали юзать string_view
+
+void  test(std::string_view str) {} // good
+
+
+int main(int argc, const char* argv) {
+
+	/*
+		Под капотом string юзает small string optimisation, т.е. под небольшие строки не выделяет память (работает в компиляторах gcc, clang.  На винде не пашит)
+
+		Псевдокод string:
+
+			class string {
+				private:
+					char small_str [10]; // например 10, если размер строки не превышал 10, то он хрнаился в массиве, иначе выделяется память
+					char* str;
+			}
+	*/
+
+	std::string str{ "123" }; 
+
+	// std::string str1{ "" }; // bad
+	// std::string str2{}; // good 
+
+
+	system("pause");
+	return 0;
+}
