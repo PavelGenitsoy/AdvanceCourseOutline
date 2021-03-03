57b8c27ea097f1e44ec8d88befab21c2575321f1
Pavel <pavlogenizoy@gmail.com>
2021-03-04 01:06:21 +0200

    added some example ostream_iterator

---
+++ ostream_iterator/Source.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <list>
+#include <iterator>
+
+
+
+int main(int argc, const char* argv) {
+
+	std::ostream_iterator<int> writer(std::cout, "\n");
+
+	*writer = 200;
+
+	++writer;
+
+	*writer = 300;
+
+
+
+
+
+
+	system("pause");
+	return 0;
+}
