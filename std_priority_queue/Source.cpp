9b44b7badc5e6b4f082f50e98a4f31c0a8b1619d
Pavel <pavlogenizoy@gmail.com>
2021-03-03 21:49:07 +0200

    added some info about priority_queue

---
+++ std_priority_queue/Source.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <queue>
+
+
+int main(int argc, const char* argv) {
+
+	std::priority_queue<int> q;
+
+	q.push(2);
+	q.push(1);
+	q.push(100);
+	q.push(6);
+	q.push(5);
+
+	std::cout << q.top() << std::endl;
+
+	q.pop();
+
+	std::cout << q.top() << std::endl;
+
+
+	system("pause");
+	return 0;
+}
