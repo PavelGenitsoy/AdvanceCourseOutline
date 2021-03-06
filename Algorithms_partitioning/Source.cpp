bea0a3b6c36a0c41294eed3697ca880476c13ec3
Pavel <pavlogenizoy@gmail.com>
2021-03-06 16:58:11 +0200

    added info about partitioning operations

---
+++ Algorithms_partitioning/Source.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <list>
+#include <iterator>
+#include <string>
+
+
+
+int main(int argc, const char* argv[]) {
+
+	std::list<int> l{ 1, 2, 2, 4 };
+	std::vector<int> v{ 1, 2, 3, 2, 5, 10 };
+
+	std::copy(v.begin(), v.end(), std::ostream_iterator<int>(std::cout, " -> "));
+	std::cout << std::endl;
+
+	auto it = std::partition(v.begin(), v.end(), [](auto value) {  // в левой половине посл-сти элементы что удовлетворяют условию, в правой - не удовлетворяют
+		return value % 2 == 0;
+	});
+
+	std::copy(v.begin(), v.end(), std::ostream_iterator<int>(std::cout, " -> "));
+	std::cout << std::endl;
+
+	std::copy(v.begin(), it, std::ostream_iterator<int>(std::cout, " -> "));
+	std::cout << std::endl;
+
+
+
+
+	system("pause");
+	return 0;
+}
