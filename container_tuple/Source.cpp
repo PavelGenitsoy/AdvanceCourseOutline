c9a67c9fdbca3f2c74efb2e957fb2c26530cbf6c
Pavel <pavlogenizoy@gmail.com>
2021-03-06 21:00:30 +0200

    added info about tuple

---
+++ container_tuple/Source.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <tuple>
+
+
+
+int main(int argc, const char* argv[]) {
+
+	//std::tuple<int, double, char> t {1, 1.f, 'c'};
+	auto t = std::make_tuple(1, 1.111f, 'c');
+
+
+	std::cout << std::get<2>(t) << std::endl; 
+
+
+	// int a1;
+	double a2;
+	char a3;
+
+	std::tie(std::ignore, a2, a3) = t; // tuple разбили.  Так делали до С++11, после стали юзать struct binding
+
+	std::cout << a2 << std::endl;
+
+
+	auto[b1, b2, b3] = t; // struct binding .  Раскручиваем и получаем значения
+	std::cout << b1 << std::endl;
+
+
+
+
+	system("pause");
+	return 0;
+}
